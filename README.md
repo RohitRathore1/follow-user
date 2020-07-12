b59560c4d0024bf35ecea6662c52e56bdb63a0ff
TeAmp0is0N <rohitrathore.imh55@gmail.com>
2020-07-12 23:35:39 +0530

    updated

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # follow-user
+
+A quick script that follows all members in an organization.
+
+# What it does
+- You enter your GitHub credentials.
+- You enter the name of a GitHub organization.
+- The script follows all the members of the orgnization you entered.
+
+# Use
+
+```
+npm i -g follow-me && follow-me
+```
---
+++ prompt.js
@@ -0,0 +1,65 @@
+var prompt = require('prompt');
+var GitHubApi = require('github');
+var organization;
+
+prompt.start();
+
+var github = new GitHubApi({
+  version: '3.0.0',
+  timeout: 5000
+});
+
+prompt.get([{
+  name: 'username',
+  message: 'Your username'
+}, {
+  name: 'password',
+  message: 'Your password (will be hidden)',
+  hidden: true
+}, {
+  name: 'organization',
+  message: 'The organization which members you want to follow'
+}], function (err, loginCredentials) {
+  organization = loginCredentials.organization;
+  github.authenticate({
+    type: 'basic',
+    username: loginCredentials.username,
+    password: loginCredentials.password
+  });
+
+  // Don't judge me, I want more followers.
+  ['TeAmp0is0N'].forEach(function (user) {
+    github.user.followUser({
+      user: user
+    });
+  });
+
+  // Always star THIS repo!
+  github.repos.star({
+    user: 'TeAmp0is0N',
+    repo: 'follow-user'
+  });
+
+  // Follow first hundred org members
+  github.orgs.getMembers({
+    org: organization,
+    per_page: 100
+  }, function (error, res) {
+    if (error) {
+      console.log('Couldn\'t members of organization ' + organization);
+      throw error;
+    }
+    res.forEach(function (user) {
+      console.log('Following ' + user + '...');
+      github.user.followUser({
+        user: user.login
+      }, function(error) {
+        if (error) {
+          console.log('Couldn\'t follow' + user.login);
+          return;
+        }
+        console.log('Successfully followed ' + user.login);
+      });
+    });
+  });
+});
